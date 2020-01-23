09afec1354e1b3fb5c4f34c6c512302b103dbe94
emilbra <36479916+emilbra@users.noreply.github.com>
2020-01-23 14:22:57 +0100

    Update dwnmsql.ps1

--- dwnmsql.ps1
+++ dwnmsql.ps1
@@ -1,3 +1,4 @@
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
 Set-ExecutionPolicy Unrestricted -Force
 $client = new-object System.Net.WebClient
 $client.DownloadFile(“https://dev.mysql.com/get/Downloads/MySQLInstaller/mysql-installer-web-community-8.0.19.0.msi”,“C:\mysql.msi”)
