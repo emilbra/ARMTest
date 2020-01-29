e7bdaf51e0d0d13de557375af2678b0b559beb8f
emilbra <36479916+emilbra@users.noreply.github.com>
2020-01-29 09:37:59 +0100

    Update dwnmsql.ps1

--- dwnmsql.ps1
+++ dwnmsql.ps1
@@ -1,5 +1,3 @@
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
-Set-ExecutionPolicy Unrestricted -Force
-$client = new-object System.Net.WebClient
-$client.DownloadFile(https://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.62-winx64.msi)
-msiexec /i C:\mysql.msi /passive
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+choco install -y  mysql
